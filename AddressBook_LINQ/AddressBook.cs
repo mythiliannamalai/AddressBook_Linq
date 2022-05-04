6a3f39af5cd4e7407133b335ca6442d5e8c643e2
mythili annamalai <mythiliannamalai45@gmail.com>
2022-05-04 20:33:30 +0530

    add contact

---
+++ AddressBook_LINQ/AddressBook.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace AddressBook_LINQ
+{
+    public class AddressBook
+    {
+        DataTable dataTable = new DataTable();
+        public AddressBook()
+        {
+            dataTable.Columns.Add("FirstName", typeof(string));
+            dataTable.Columns.Add("LastName", typeof(string));
+            dataTable.Columns.Add("Address", typeof(string));
+            dataTable.Columns.Add("City", typeof(string));
+            dataTable.Columns.Add("State", typeof(string));
+            dataTable.Columns.Add("Zip", typeof(int));
+            dataTable.Columns.Add("PhoneNumber", typeof(long));
+            dataTable.Columns.Add("Email", typeof(string));
+        }
+        public void AddContact()
+        {
+            dataTable.Rows.Add("Mythili", "Annamalai", "nadu st", "salem", "tamilnady",636002,9600474622,"mythili@gmail.com");
+            dataTable.Rows.Add("Naveen kumar", "Selvaraj", "nadu st", "salem", "tamilnady", 636002, 9600474623, "naveen@gmail.com");
+            dataTable.Rows.Add("Kiruba", "Selvaraj", "nadu st", "salem", "tamilnady", 636002, 9600474624, "kiruba@gmail.com");
+            dataTable.Rows.Add("Lavanya", "Annamalai", "jj st", "salem", "tamilnady", 636002, 9600474625, "lavanya@gmail.com");
+            dataTable.Rows.Add("Santhosh", "Annamalai", "jj st", "salem", "tamilnady", 636002, 9600474626, "santhosh@gmail.com");
+            Console.WriteLine("Contact is Added in Address Book ");
+        }        
+    }
+}
---
+++ AddressBook_LINQ/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data;
+
+namespace AddressBook_LINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Address Book Using LINQ");
+            AddressBook addressbook = new AddressBook();
+            int option = 0;
+            do
+            {
+                Console.WriteLine("1: For Add the Contact");               
+                Console.WriteLine("0: For Exist");
+                Console.WriteLine("Enter your option :");
+                option = int.Parse(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        addressbook.AddContact();
+                        break;                    
+                    case 0:
+                        Console.WriteLine("Exit");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Input try again..");
+                        break;
+                }
+            }
+            while (option != 0);
+        }
+    }
+}
